e8497c5005aa50bdfa17fd881614913e491b0f96
Qing Sun <qsun@salesforce.com>
2018-09-22 12:53:13 -0700

    fixed port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 express = require('express');
 const app = express();
 app.use(express.static('static'));
-app.listen(3000, function(){
-    console.log('App started on port 3000');
+var port = process.env.PORT || 8080;
+app.listen(port, function(){
+    console.log('App started on port ', port);
 });
