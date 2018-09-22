aafb1688e64fd710de3f41a6aa048c4ff22d411f
Qing Sun <qsun@salesforce.com>
2018-09-22 15:59:45 -0700

    serving /public

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 express = require('express');
 const app = express();
-app.use(express.static('static'));
+
+//app.use(express.static('static'));
+var serveIndex = require('serve-index');
+app.use('/public', serveIndex('static')); // shows you the file list
+app.use('/public', express.static('static')); // serve the actual files
+
 var port = process.env.PORT || 8080;
 app.listen(port, function(){
     console.log('App started on port ', port);
