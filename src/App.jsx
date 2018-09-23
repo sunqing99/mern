f8d81ea8e124984d6de4eabffaf9fd46394e16c9
Qing Sun <qsun@salesforce.com>
2018-09-22 20:43:09 -0700

    no polyfill

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const contentNode = document.getElementById('contents');
 const continents = ['Africa','America','Asia','Australia','Europe'];
-const message = continents.map(c => `Hello ${c}`).join(' ');
+const message = `Hello ${continents.find(c => c === 'Asia')}`
 // var component = <h1 className="green">Hello World!</h1>; // simple JSX cmp
 const component = <p>{message}</p>;
 ReactDOM.render(component, contentNode);
--- static/App.js
+++ static/App.js
@@ -2,9 +2,9 @@
 
 var contentNode = document.getElementById('contents');
 var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-var message = continents.map(function (c) {
-  return 'Hello ' + c;
-}).join(' ');
+var message = 'Hello ' + continents.find(function (c) {
+  return c === 'Asia';
+});
 // var component = <h1 className="green">Hello World!</h1>; // simple JSX cmp
 var component = React.createElement(
   'p',
