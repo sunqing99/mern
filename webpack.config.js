80a893eab57a55c18439512e12f66ff1aa6dd371
Qing Sun <qsun@salesforce.com>
2018-09-29 16:26:07 -0700

    webpack config tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
+// check out https://stackoverflow.com/questions/48985780/webpack-4-create-vendor-chunk/48986526#48986526
 const path = require('path');
 module.exports = {
     mode: 'production',
     entry: {
         app: './src/App.jsx',
-        vendor: ['react', 'react-dom', 'whatwg-fetch'],
+        vendor: ['react', 'react-dom', 'whatwg-fetch', 'babel-polyfill'],
     },
     output: {
         path: path.resolve(__dirname, 'static'),
-        filename: '[name].bundle.js'
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js',
     },
     optimization: {
         splitChunks: {
             cacheGroups: {
                 vendor: {
-                    test: /[\\/]node_modules[\\/](react|react-dom|whatwg-fetch)[\\/]/,
+                    chunks: 'initial',
                     name: 'vendor',
-                    chunks: 'all',
-                }
+                    test: 'vendor',
+                    enforce: true
+                },
             }
-        }
+        },
     },
     module: {
         rules: [
