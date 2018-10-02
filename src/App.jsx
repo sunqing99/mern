0444bfeefcf20374d7ab93a3c51c8449813ada45
Qing Sun <qsun@salesforce.com>
2018-10-02 13:00:16 -0700

    testing nested branch, not yet working

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,49 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
   HashRouter as Router, Route, Switch, Redirect, withRouter,
 } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import IssueList from './IssueList';
 import IssueEdit from './IssueEdit';
 
 const contentNode = document.getElementById('contents');
 const NoMatch = () => <p>Page Not Found (App.jsx)</p>;
 
+const App = ({ children }) => {
+  console.log('Children:', JSON.stringify(children, null, 2));
+  return (
+    <div>
+      <div className="header">
+        <h1>Issue Tracker</h1>
+      </div>
+      <div className="contents">
+        {children}
+      </div>
+      <div className="footer">
+        Full source code available at this <a href="https://github.com/sunqing99/mern/tree/chap8">GitHub repository</a>
+      </div>
+    </div>
+  )
+};
+
+App.propTypes = {
+  children: PropTypes.objectOf(React.component).isRequired,
+};
+
 const RoutedApp = () => (
   <Router>
-    <Switch>
-      <Redirect exact from="/" to="/issues" />
-      <Route exact path="/issues" component={withRouter(IssueList)} />
-      <Route path="/issues/:id" component={IssueEdit} />
-      <Route component={NoMatch} />
-    </Switch>
+
+    <Route exact path="/" component={App}>
+      <Switch>
+        <Redirect exact from="/" to="/issues" />
+        <Route exact path="/issues" component={withRouter(IssueList)} />
+        <Route path="/issues/:id" component={IssueEdit} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Route>
+
   </Router>
 );
 
